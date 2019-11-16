31b6af5b38e73ea154b76530a733bff6e0931a60
GameTitan <saraf.siddharth@gmail.com>
2019-11-16 15:34:48 -0800

    'Adding'

---
+++ urlShortener/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+	path('', views.home, name="home"),
+	path('myUrls/', views.url_list, name="url_list"),
+	path('redirect/<str:key>/', views.redirect, name="redirect"),
+	path('new/', views.new_url, name='new_url'),
+	path('edit/<str:key>/', views.edit_url, name="edit_url"),
+	path('delete/<str:key>/', views.del_url, name="del_url"),
+    path('accounts/', include('django.contrib.auth.urls'), name="login"),
+    path('accounts/signup/', views.signup, name="signup"),
+    path('accounts/edit', views.edit_account, name="edit_account"),
+]
